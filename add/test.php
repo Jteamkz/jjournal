4789a0d38fe9326b48c1ff21f72261f875336d39
Osman <osmannamso123@gmail.com>
2017-11-09 00:00:08 +0600

    Summary
    Description

--- add/test.php
+++ add/test.php
@@ -12,7 +12,7 @@
 		$(document).ready(function(){
 		$("#ewevopros").click(function(){
 			 a++;
-   			 $("#voprosP").append("<h5>Вопрос "+(a+1)+"</h5><input type='text' name='vopros"+a+"' placeholder='Вопрос'><br><input type='text' class='rightanswer' name='right"+a+"' placeholder='Правильный ответ'><input class='wronganswer' type='text' name='wrong"+a+"0' placeholder='Неправильный ответ'><input class='wronganswer' type='text' name='wrong"+a+"1' placeholder='Неправильный ответ'><input class='wronganswer' type='text' name='wrong"+a+"2' placeholder='Неправильный ответ'>");
+   			 $("#voprosP").append("<h5 id='slovo"+a+"''>Вопрос "+(a+1)+"</h5><input type='text' name='vopros"+a+"' placeholder='Вопрос'><br id='br"+a+"'><input type='text' class='rightanswer' name='right"+a+"' placeholder='Правильный ответ'><input class='wronganswer' type='text' name='wrong"+a+"0' placeholder='Неправильный ответ'><input class='wronganswer' type='text' name='wrong"+a+"1' placeholder='Неправильный ответ'><input class='wronganswer' type='text' name='wrong"+a+"2' placeholder='Неправильный ответ'>");
 		});
 	});
 			
@@ -36,15 +36,28 @@
 			})
 		
 	});
+	$("#delete").click(function(){
+		if(a > 0){
+			$("#br"+a).remove();
+			$("#slovo"+a).remove();
+			$("input[name=vopros"+a+"]").remove();
+			$("input[name=right"+a+"]").remove();
+			$("input[name=wrong"+a+"0]").remove();
+			$("input[name=wrong"+a+"1]").remove();
+			$("input[name=wrong"+a+"2]").remove();
+			a = a - 1;
+		}
+	});
 	});
 	</script>
 	<br><button id="ewevopros">Еще</button>
+	<button id="delete">Удалить</button>
 	<form method='post' id="voprosForm">
 	<input type='text' name='name' placeholder='Название'>
 	<input type='text' name='description' placeholder='Описание'>
-		<h5>Вопрос 1</h5>
+		<h5 id="slovo0">Вопрос 1</h5>
 		<input type='text' name='vopros0' placeholder='Вопрос'>
-		<br><input type='text' name='right0' placeholder='Правильный ответ' class="rightanswer">
+		<br id="br0"><input type='text' name='right0' placeholder='Правильный ответ' class="rightanswer">
 			<input type='text' name='wrong00' placeholder='Неправильный ответ' class="wronganswer"><input type='text' name='wrong01' placeholder='Неправильный ответ' class="wronganswer"><input type='text' name='wrong02' placeholder='Неправильный ответ' class="wronganswer">
 		<p id="voprosP"></p>
 	<br><button id="addvopros">Add</button></form>
--- admin_studens.php
+++ admin_studens.php
@@ -13,7 +13,6 @@ include 'php/db/get_query.php';
 <html>
 <head>
 	<title>Студенты</title>
-
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
